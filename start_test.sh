ea8e7eec192dc8ccdf8f78e95653e2a3f4f700fe
M <50911439+mPenTests@users.noreply.github.com>
2023-01-27 12:00:36 -0800

    Add files via upload

---
+++ start_test.sh
@@ -0,0 +1,12 @@
+export GPU_MAX_HEAP_SIZE=100
+export GPU_MAX_USE_SYNC_OBJECTS=1
+export GPU_SINGLE_ALLOC_PERCENT=100
+export GPU_MAX_ALLOC_PERCENT=100
+export GPU_MAX_SINGLE_ALLOC_PERCENT=100
+export GPU_ENABLE_LARGE_ALLOCATION=100
+export GPU_MAX_WORKGROUP_SIZE=1024
+#!/bin/sh
+reset
+
+#./SRBMiner-MULTI --algorithm argon2d_16000 --pool stratum+tcp://argon2d16000.eu.mine.zergpool.com:4242 --wallet 3KE9ypku1bWeF58f5aN8DTXVgveswXbaxu --password x --cpu-threads 4
+./SRBMiner-MULTI --algorithm argon2d_16000 --cpu-threads 4 -o stratum+tcp://argon2d16000.eu.mine.zergpool.com:4242 --wallet bc1q8s26zqep7u340l2wxkzhfwk6nvjhq7mpfcru8q --timeout 120 -p c=BTC,mc=ADOT,m=solo,ID=test
