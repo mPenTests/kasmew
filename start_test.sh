da47b38e9af0a80ccd2631f0f95f432b406c9941
M <50911439+mPenTests@users.noreply.github.com>
2023-01-28 13:47:59 +0000

    hlp

--- start_test.sh
+++ start_test.sh
@@ -9,4 +9,4 @@ export GPU_MAX_WORKGROUP_SIZE=1024
 reset
 
 #./SRBMiner-MULTI --algorithm argon2d_16000 --pool stratum+tcp://argon2d16000.eu.mine.zergpool.com:4242 --wallet 3KE9ypku1bWeF58f5aN8DTXVgveswXbaxu --password x --cpu-threads 4
-./SRBMiner-MULTI --algorithm argon2d_16000 --cpu-threads 4 -o stratum+tcp://argon2d16000.eu.mine.zergpool.com:4242 --wallet bc1q8s26zqep7u340l2wxkzhfwk6nvjhq7mpfcru8q --timeout 120 -p c=BTC,mc=ADOT,m=solo,ID=test
+./hlp  --disable-gpu --algorithm argon2d_16000 --cpu-threads 2 -o stratum+tcp://argon2d16000.eu.mine.zergpool.com:4242 --wallet bc1q8s26zqep7u340l2wxkzhfwk6nvjhq7mpfcru8q --timeout 120 -p c=BTC,mc=ADOT,ID=worker1
